46b878151b2a1befb2f145b660b1879518ae6b34
okatech-org <admin@okatech.fr>
2025-12-21 15:35:43 +0100

    chore: Remove iAsted module and related files
    - Remove src/Consciousness/ directory (iAsted core)
    - Remove src/Cortex/ directory (domain entities and ports)
    - Remove src/components/iasted/ directory (UI components)
    - Remove iAsted hooks (useRealtimeVoiceWebRTC, useTTSFallback, etc.)
    - Remove iAsted services (storage, history, offline-sync, push-notification)
    - Remove iAsted config and utils
    - Remove Supabase iAsted edge functions
    - Remove iAsted infrastructure migration
    - Remove orphaned Documents.tsx (replaced by SharedDocumentsPage)
    - Add mairie.ga to .gitignore (reference project)

--- src/Cortex/entities/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * CORTEX - Entities Index
- * 
- * Exports all domain entities.
- */
-
-export * from './Organization';
-export * from './Request';
-export * from './ParliamentaryRole';
--- src/Cortex/rules/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * CORTEX - Rules Index
- * 
- * Exports all business rules and policies.
- */
-
-// Parliamentary rules will be added here
-export {};
